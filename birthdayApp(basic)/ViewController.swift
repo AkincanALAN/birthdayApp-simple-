1a231788df861ae541c5757b609455263a6576e7
AkincanALAN <alanakincan@gmail.com>
2024-02-09 22:25:31 +0300

    App without delete button and "if let"

--- birthdayApp(basic)/ViewController.swift
+++ birthdayApp(basic)/ViewController.swift
@@ -8,20 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedBirthday = UserDefaults.standard.object(forKey: "birthday")
+        nameLabel.text = storedName as? String
+        birthdayLabel.text = storedBirthday as? String
+        
     }
-
-
+    
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var birthdayText: UITextField!
+    
     @IBAction func saveButton(_ sender: Any) {
-        nameLabel.text = nameText.text
-        birthdayLabel.text = birthdayText.text
+        UserDefaults.standard.set(nameText.text, forKey: "name")
+        UserDefaults.standard.set(birthdayText.text, forKey: "birthday")
+        nameLabel.text = "Name: \(nameText.text!) saved"
+        birthdayLabel.text = "Date: \(birthdayText.text!) saved"
+
     }
+    
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var birthdayLabel: UILabel!
-    
 }
